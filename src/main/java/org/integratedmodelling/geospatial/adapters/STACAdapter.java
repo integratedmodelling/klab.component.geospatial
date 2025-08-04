c9c96aac61af24737f38f6a3bd65583a8bfb1d12
Íñigo Cobián <inigo.cobian@gmail.com>
2025-08-05 00:38:28 +0200

    Create first version of STAC encoder

--- src/main/java/org/integratedmodelling/geospatial/adapters/STACAdapter.java
+++ src/main/java/org/integratedmodelling/geospatial/adapters/STACAdapter.java
@@ -1,22 +1,45 @@
 package org.integratedmodelling.geospatial.adapters;
 
 import kong.unirest.json.JSONObject;
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.hortonmachine.gears.io.stac.HMStacCollection;
+import org.hortonmachine.gears.io.stac.HMStacItem;
+import org.hortonmachine.gears.io.stac.HMStacManager;
+import org.hortonmachine.gears.libs.modules.HMRaster;
+import org.hortonmachine.gears.libs.monitor.LogProgressMonitor;
+import org.hortonmachine.gears.utils.RegionMap;
+import org.hortonmachine.gears.utils.geometry.GeometryUtilities;
+import org.integratedmodelling.geospatial.adapters.raster.RasterEncoder;
 import org.integratedmodelling.geospatial.adapters.stac.STACManager;
+import org.integratedmodelling.geospatial.adapters.stac.STACUtils;
 import org.integratedmodelling.klab.api.collections.Parameters;
 import org.integratedmodelling.klab.api.data.Data;
 import org.integratedmodelling.klab.api.data.Version;
+import org.integratedmodelling.klab.api.exceptions.KlabIllegalStateException;
+import org.integratedmodelling.klab.api.exceptions.KlabResourceAccessException;
 import org.integratedmodelling.klab.api.exceptions.KlabUnimplementedException;
 import org.integratedmodelling.klab.api.geometry.Geometry;
 import org.integratedmodelling.klab.api.knowledge.Artifact;
 import org.integratedmodelling.klab.api.knowledge.Observable;
 import org.integratedmodelling.klab.api.knowledge.Resource;
 import org.integratedmodelling.klab.api.knowledge.Urn;
+import org.integratedmodelling.klab.api.knowledge.observation.scale.Scale;
+import org.integratedmodelling.klab.api.knowledge.observation.scale.space.Space;
+import org.integratedmodelling.klab.api.knowledge.observation.scale.time.Time;
 import org.integratedmodelling.klab.api.scope.Scope;
 import org.integratedmodelling.klab.api.services.resources.adapters.Parameter;
 import org.integratedmodelling.klab.api.services.resources.adapters.ResourceAdapter;
-import org.integratedmodelling.klab.api.services.resources.impl.ParameterImpl;
+import org.integratedmodelling.klab.api.services.runtime.Notification;
+import org.integratedmodelling.klab.runtime.scale.space.EnvelopeImpl;
+import org.integratedmodelling.klab.runtime.scale.space.ProjectionImpl;
+import org.opengis.coverage.grid.GridCoverage;
 
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * STAC is service-bound so it can be embedded in a runtime.
@@ -52,8 +75,108 @@ public class STACAdapter {
     }
 
     @ResourceAdapter.Encoder
-    public void encode(Urn urn, Data.Builder builder, Geometry geometry, Observable observable, Scope scope) {
-        // TODO
+    public void encode(Resource resource, Urn urn, Data.Builder builder, Geometry geometry, Observable observable, Scope scope) {
+        String collectionUrl = resource.getParameters().get("collection", String.class);
+        JSONObject collectionData = STACManager.requestMetadata(collectionUrl, "collection");
+        String collectionId = collectionData.getString("id");
+        String catalogUrl = STACUtils.getCatalogUrl(collectionUrl, collectionId, collectionData);
+        JSONObject catalogData = STACManager.requestMetadata(catalogUrl, "catalog");
+        String assetId = resource.getParameters().get("asset", String.class);
+
+        boolean hasSearchOption = STACUtils.containsLinkTo(catalogData, "search");
+        if (!hasSearchOption) {
+            throw new KlabUnimplementedException("Static catalogs are not implemented yet");
+        }
+
+        var space = (Space) geometry.getDimensions().stream().filter(d -> d instanceof Space)
+                .findFirst().orElseThrow();
+        var envelope = space.getEnvelope();
+        List<Double> bbox =  List.of(envelope.getMinX(), envelope.getMaxX(), envelope.getMinY(), envelope.getMaxY());
+        var time = (Time) geometry.getDimensions().stream().filter(d -> d instanceof Time)
+                .findFirst().orElseThrow();
+        var resourceTime = (Time) Scale.create(resource.getGeometry()).getTime();
+
+        LogProgressMonitor lpm = new LogProgressMonitor();
+        HMStacManager manager = new HMStacManager(catalogUrl, lpm);
+        HMStacCollection collection = null;
+        try {
+            manager.open();
+            collection = manager.getCollectionById(resource.getParameters().get("collectionId", String.class));
+        } catch (Exception e) {
+            throw new KlabResourceAccessException("Cannot access to STAC collection " + collectionUrl);
+        }
+
+        if (collection == null) {
+            throw new KlabResourceAccessException("Collection " + resource.getParameters().get("collection", String.class) + " cannot be found.");
+        }
+
+        // TODO for now, we do not manage the semantics for the MergeMode
+        HMRaster.MergeMode mergeMode = HMRaster.MergeMode.SUM;
+        /*
+        IObservable targetSemantics = scope.getTargetArtifact() instanceof Observation
+                ? ((Observation) scope.getTargetArtifact()).getObservable()
+                : null;
+        HMRaster.MergeMode mergeMode = chooseMergeMode(targetSemantics, scope.getMonitor());
+         */
+
+        var env = EnvelopeImpl.create(envelope.getMinX(), envelope.getMaxX(), envelope.getMinY(), envelope.getMaxY(), space.getProjection());
+        var poly = GeometryUtilities.createPolygonFromEnvelope(env.getJTSEnvelope());
+        collection.setGeometryFilter(poly);
+
+        // TODO check how to validate the time coverage
+        var start = time.getStart();
+        var end = time.getEnd();
+        collection.setTimestampFilter(new Date(start.getMilliseconds()), new Date(end.getMilliseconds()));
+
+
+        GridCoverage2D coverage = null;
+        try {
+            List<HMStacItem> items = collection.searchItems();
+
+            if (items.isEmpty()) {
+                manager.close();
+                throw new KlabIllegalStateException("No STAC items found for this context.");
+            }
+            builder.notification(Notification.debug("Found " + items.size() + " STAC items."));
+
+            if (mergeMode == HMRaster.MergeMode.SUBSTITUTE) {
+                sortByDate(items, builder);
+            }
+
+            // TODO check the usage of space.getStandardizedHeight();
+            RegionMap region = RegionMap.fromBoundsAndGrid(space.getEnvelope().getMinX(), space.getEnvelope().getMaxX(),
+                    space.getEnvelope().getMinY(), space.getEnvelope().getMaxY(), (int) space.getStandardizedWidth(),
+                    (int) space.getStandardizedHeight());
+
+            ReferencedEnvelope regionEnvelope = new ReferencedEnvelope(region.toEnvelope(), ((ProjectionImpl)space.getProjection()).getCRS());
+            RegionMap regionTransformed = RegionMap.fromEnvelopeAndGrid(regionEnvelope, (int) space.getStandardizedWidth(),
+                    (int) space.getStandardizedHeight());
+            Set<Integer> EPSGsAtItems = items.stream().map(HMStacItem::getEpsg).collect(Collectors.toUnmodifiableSet());
+            if (EPSGsAtItems.size() > 1) {
+                builder.notification(Notification.warning("Multiple EPSGs found on the items " + EPSGsAtItems.toString() + ". The transformation process could affect the data."));
+            }
+
+            // Forget about AWS for now
+
+            // Allow transform ensures the process to finish, but I would not bet on the resulting
+            // data.
+            final boolean allowTransform = true;
+            HMRaster outRaster = collection.readRasterBandOnRegion(regionTransformed, assetId, items, allowTransform, mergeMode, lpm);
+            coverage = outRaster.buildCoverage();
+            manager.close();
+        } catch (Exception e) {
+            throw new KlabResourceAccessException("Cannot build STAC raster output. Reason " + e.getMessage());
+        }
+        RasterEncoder.INSTANCE.encodeFromCoverage(resource, Parameters.create(urn.getParameters()), coverage, geometry, builder, observable, scope);
+    }
+
+    private void sortByDate(List<HMStacItem> items, Data.Builder builder) {
+        if (items.stream().anyMatch(i -> i.getTimestamp() == null)) {
+            throw new KlabIllegalStateException("STAC items are lacking a timestamp and could not be sorted by date.");
+        }
+        items.sort(Comparator.comparing(HMStacItem::getTimestamp));
+        builder.notification(Notification.debug(
+                "Ordered STAC items. First: [" + items.get(0).getTimestamp() + "]; Last [" + items.get(items.size() - 1).getTimestamp() + "]"));
     }
 
     /**
